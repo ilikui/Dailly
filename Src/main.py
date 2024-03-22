21e7ca760d77d9f038c103f75b97dbd039e9900d
ilikui <2563043733@qq.com>
2024-03-22 19:27:17 +0800

    update by likui

--- Src/main.py
+++ Src/main.py
@@ -4,13 +4,9 @@ import requests
 
 
 
-# 信息分发
-
-
-
-import requests
-def send(message:str):
-    url = "机器人webhook地址"
+def WeChatRobot(message:str):
+    apiurl ='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key='
+    key = os.environ["WEBHOOK_KEY"]
     headers = {
         "Content-Type": "application/json"
     }
@@ -21,24 +17,26 @@ def send(message:str):
             "mentioned_list": ["@all"]
         }
     }
+
+    url = apiurl+key
+
+    print(url)
+    
     response = requests.post(url, headers=headers, json=data)
     records = response.json()
     return records
-def sendAll(message:str):
-    url = "机器人webhook地址"
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = {
-        "msgtype": "text",
-        "text": {
-            "content": message,
-            "mentioned_list": ["@all"]
-        }
-    }
-    response = requests.post(url, headers=headers, json=data)
-    records = response.json()
-    return records
+
+
+
+
+if __name__ == '__main__':
+    
+    WeChatRobot('大家好，我是mimi')
+
+
+    
+
+
 
 
 
