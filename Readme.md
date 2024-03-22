c45897b35bfd63b158ac7a297119b00eabf331a9
ilikui <2563043733@qq.com>
2024-03-22 18:08:44 +0800

    update by likui

--- Readme.md
+++ Readme.md
@@ -9,4 +9,22 @@
 - [ ] 报警提醒
 
 
+## 需要做的事情
+
+* 采集网络的数据
+  * 使用RSS订阅源
+  * 数据以json格式存储
+* 数据归类
+* 数据可视化大屏
+  * JS 解析json数据可视化现实
+
+
+
+## 信息分发接口
+
+* 微信Robot 
+  * 帮助文档 https://developer.work.weixin.qq.com/document/path/91770
+  * https://chatbot.weixin.qq.com/login
+* 邮箱
+* 手机push推送
 
--- Src/main.py
+++ Src/main.py
@@ -4,5 +4,45 @@ import requests
 
 
 
+# 信息分发
+
+
+
+import requests
+def send(message:str):
+    url = "机器人webhook地址"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": message,
+            "mentioned_list": ["@all"]
+        }
+    }
+    response = requests.post(url, headers=headers, json=data)
+    records = response.json()
+    return records
+def sendAll(message:str):
+    url = "机器人webhook地址"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": message,
+            "mentioned_list": ["@all"]
+        }
+    }
+    response = requests.post(url, headers=headers, json=data)
+    records = response.json()
+    return records
+
+
+
+
+
 
 print("===================================")
